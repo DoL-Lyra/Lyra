65e9a2583c97ff584d48f95b5f7bafde89948756
Sakari <sk308269317@outlook.com>
2023-10-17 14:23:42 +0800

    refactor: params

--- gen-comb.py
+++ gen-comb.py
@@ -14,8 +14,9 @@ recommend_dec = [15,31]
 
 baseurl_github = "https://github.com/sakarie9/DOL-CHS-MODS/releases/download/"
 baseurl_ghproxy = f"https://ghproxy.com/{baseurl_github}"
-pair_path = str(sys.argv[1])
-release_tag = str(sys.argv[2])
+#pair_path = str(sys.argv[1])
+pair_path = "paris"
+release_tag = str(sys.argv[1])
 md_path = "content/posts/downloads"
 
 release_fontmatter = f"""
