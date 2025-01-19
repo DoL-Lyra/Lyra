8d87febc96d1b92823b658917ecc6fa9459db8f6
lyrae-a <176460279+lyrae-a@users.noreply.github.com>
2025-01-19 21:52:33 +0800

    update gen-comb.py

--- gen-comb.py
+++ gen-comb.py
@@ -4,20 +4,31 @@ import os
 import datetime
 
 # 功能定义
-functions = ["UCB", "SUSATO", "WAX", "KR特写", "BJ特写", "CSD", "作弊", "BESC", "BES"]
+functions = [
+    "UCB",
+    "SUSATO",
+    "WAX",
+    "HIKARI",
+    "KR特写",
+    "BJ特写",
+    "CSD",
+    "作弊",
+    "BESC",
+]
 
+# 暂时移除： 二进制:   10001000, 十进制: 136, 功能: ***CSD+SUSATO(推荐)***, 推荐： 1
 # 白名单
-add_dec = [3,11,91,136]
+add_dec = [5, 37]
 # 黑名单
-skip_dec = [0,67,128]
+skip_dec = []
 # 推荐
-recommend_dec = [91,136]
+recommend_dec = [5, 37]
 # polyfill
-polyfill_comb = "polyfill_31"
+polyfill_comb = "polyfill_6"
 
 baseurl_github = "https://github.com/DoL-Lyra/Lyra/releases/download/"
 baseurl_ghproxy = f"https://mirror.ghproxy.com/{baseurl_github}"
-#pair_path = str(sys.argv[1])
+# pair_path = str(sys.argv[1])
 pair_path = "pairs"
 release_tag = str(sys.argv[1])
 
@@ -63,6 +74,10 @@ release_prepend = """
 <br>
 从 `v0.5.2.8` 开始， DoL 原版修改了打包框架，使得覆盖安装会造成存档丢失。本仓库内的安卓版已修改为新包名，请在旧版导出存档然后在新版中导入。
 {{< /alert >}}
+<br>
+{{< alert >}}
+BJ，KR 及 Susato 暂未适配新版，暂时移除
+{{< /alert >}}
 
 ## 汉化整合
 """
@@ -72,6 +87,7 @@ release_prepend = """
 # title = '{release_tag}'
 # +++
 # """
+
 
 class combination:
     binary = 0
@@ -86,10 +102,11 @@ class combination:
     def __lt__(self, other):
         return self.decimal < other.decimal
 
+
 combinations = []
 
-def gencomb():
 
+def gencomb():
     global functions
     global combinations
     global add_dec
@@ -98,53 +115,71 @@ def gencomb():
 
     BINARY_FORMAT = f"{len(functions):02}b"
 
-    for i in range(2**len(functions)):
+    for i in range(2 ** len(functions)):
         binary = format(i, BINARY_FORMAT)  # 用8位二进制表示
         decimal = i
 
         # A依赖于B,B是A的依赖
-        binary_and = lambda A: decimal & (1 << (A-1))
-        should_skip_dep = lambda A,B: binary_and(A) and not binary_and(B)
-        should_skip_mutex = lambda A,B: binary_and(A) and binary_and(B)
+        def binary_and(A):
+            return decimal & 1 << A - 1
+
+        def should_skip_dep(A, B):
+            return binary_and(A) and not binary_and(B)
+
+        def should_skip_mutex(A, B):
+            return binary_and(A) and binary_and(B)
+
         # 跳过 0
         if i == 0:
             continue
+        # 跳过 WAX，BJ，KR
+        if binary_and(7) or binary_and(5) or binary_and(4):
+            continue
+        # 跳过 SUSATO
+        if binary_and(8):
+            continue
         # 添加限制条件：BESC只能在BES应用的同时应用
-        if should_skip_dep(2,1):
-            continue
+        # if should_skip_dep(2, 1):
+        #     continue
         # 只存在BES不存在BESC时跳过
-        if should_skip_dep(1,2):
-            continue
+        # if should_skip_dep(1, 2):
+        #     continue
         # 特写WAX依赖BESC
-        if should_skip_dep(5,2):
+        if should_skip_dep(4, 1):
             continue
-        if should_skip_dep(6,2):
+        if should_skip_dep(5, 1):
             continue
-        if should_skip_dep(7,2):
+        if should_skip_dep(6, 1):
+            continue
+        if should_skip_dep(7, 1):
             continue
         # UCB依赖BESC
-        if should_skip_dep(9,2):
+        if should_skip_dep(9, 1):
             continue
         # 绑定WAX和BESC
-        if (binary_and(7) and not binary_and(2)) or (not binary_and(7) and binary_and(2)):
-            continue
+        # if (binary_and(7) and not binary_and(2)) or (
+        #     not binary_and(7) and binary_and(2)
+        # ):
+        #     continue
         # 特写互斥
-        if should_skip_mutex(5,6):
+        if should_skip_mutex(4, 5):
             continue
         # HP作弊异或
-        if (binary_and(3) and not binary_and(4)) or (not binary_and(3) and binary_and(4)):
+        if (binary_and(2) and not binary_and(3)) or (
+            not binary_and(2) and binary_and(3)
+        ):
             continue
         # Susato 与 BESC 互斥
-        if should_skip_mutex(8,2):
+        if should_skip_mutex(8, 1):
             continue
 
-        #combinations.append((binary, decimal))
+        # combinations.append((binary, decimal))
         combinations.append(combination(binary, decimal))
 
     # 白名单
     for dec in add_dec:
         binary = format(dec, BINARY_FORMAT)
-        #combinations.append((binary, dec))
+        # combinations.append((binary, dec))
         combinations.append(combination(binary, dec))
         combinations.sort()
     # 黑名单
@@ -152,17 +187,17 @@ def gencomb():
     combinations = combs_tmp
 
     # 功能组合
-    #for binary, decimal in combinations:
+    # for binary, decimal in combinations:
     for i, comb in enumerate(combinations):
         decimal = comb.decimal
         binary = comb.binary
 
         funcs = ""
-        for j in range(len(binary)-1,-1,-1):
+        for j in range(len(binary) - 1, -1, -1):
             # 跳过BES如果BESC存在
-            if j == len(binary)-1 and binary[j-1] == '1':
-                continue
-            if binary[j] == '1':
+            # if j == len(binary) - 1 and binary[j - 1] == "1":
+            #     continue
+            if binary[j] == "1":
                 funcs = funcs + functions[j] + "+"
         funcs = funcs.strip("+")
         # 推荐
@@ -181,11 +216,14 @@ def gencomb():
         binary = comb.binary
         functions = comb.functions
         recommend = comb.recommend
-        print(f"二进制: {binary}, 十进制: {decimal}, 功能: {functions}, 推荐： {recommend}")
+        print(
+            f"二进制: {binary}, 十进制: {decimal}, 功能: {functions}, 推荐： {recommend}"
+        )
 
     # 打印组合十进制数组
-    
-    print(sorted([ f.decimal for f in combinations ]))
+
+    print(sorted([f.decimal for f in combinations]))
+
 
 def gentable():
     global combinations
@@ -201,7 +239,7 @@ def gentable():
         def __init__(self, functions, decimal):
             self.functions = functions
             self.decimal = decimal
-        
+
     table_matrix = []
 
     # 添加 polyfill
@@ -213,7 +251,9 @@ def gentable():
     lineapk = fapk.readline()
     fapk.close()
 
-    table_matrix.append(["BESC+CSD+BJ特写(兼容版)",link_builder(linezip),link_builder(lineapk)])
+    table_matrix.append(
+        ["BESC+CSD(兼容版)", link_builder(linezip), link_builder(lineapk)]
+    )
     # 添加 polyfill 结束
 
     for comb in combinations:
@@ -225,13 +265,14 @@ def gentable():
         lineapk = fapk.readline()
         fapk.close()
 
-        table_matrix.append([comb.functions,link_builder(linezip),link_builder(lineapk)])
+        table_matrix.append(
+            [comb.functions, link_builder(linezip), link_builder(lineapk)]
+        )
 
     writer = MarkdownTableWriter(
         # table_name="汉化整合",
         headers=["版本选择", "ZIP", "APK"],
         value_matrix=table_matrix,
-        
     )
 
     md_path_now = md_path + "/" + release_tag + ".md"
@@ -249,12 +290,13 @@ def gentable():
     # f.write(writer.dumps())
     # f.close()
 
+
 def link_builder(filename):
     global baseurl_github
     global baseurl_ghproxy
     global release_tag
-    link_github = baseurl_github+release_tag+"/"+filename
-    link_ghproxy = baseurl_ghproxy+release_tag+"/"+filename
+    link_github = baseurl_github + release_tag + "/" + filename
+    link_ghproxy = baseurl_ghproxy + release_tag + "/" + filename
     str = f"[Github下载]({link_github}) / [备链]({link_ghproxy})"
     return str
 
